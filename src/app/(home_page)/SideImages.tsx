43fef682b963a524abb5cec9f78bf081b24c5f5c
Casey Mcgrath <caseymcgrath1000@gmail.com>
2025-02-06 22:06:56 -0500

    Readding 3 images to home page

--- src/app/(home_page)/SideImages.tsx
+++ src/app/(home_page)/SideImages.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 
 export default function SideImages() {
   return (
-    <div className="hidden w-full lg:pt-24 md:block">
+    <div className="hidden w-full md:block lg:pt-24">
       <div className="flex items-center justify-start gap-4">
-        <div className="lg:w-44 w-28 flex-none space-y-8 pt-44">
+        <div className="w-28 flex-none space-y-8 pt-44 lg:w-44">
           <div className="relative flex justify-center">
             <Image
               src="/homeTiles/Batteries.jpg"
@@ -28,7 +28,7 @@ export default function SideImages() {
             />
           </div>
         </div>
-        <div className="lg:w-44 w-28 flex-none space-y-8">
+        <div className="w-28 flex-none space-y-8 lg:w-44">
           <div className="relative">
             <Image
               src="/homeTiles/airplane.jpg"
@@ -51,6 +51,42 @@ export default function SideImages() {
               className="rounded-xl bg-gray-900/5 object-cover shadow-lg"
             />
           </div>
+          </div>
+          <div className="lg:w-44 w-28 flex-none space-y-8">
+            <div className="relative">
+              <Image
+                src="/homeTiles/manufacturing.webp"
+                alt="Manufacturing"
+                width={400}
+                height={400}
+                priority={false} // Do not preload
+                placeholder="empty" // No blur
+                className="rounded-xl bg-gray-900/5 object-cover shadow-lg"
+              />
+            </div>
+            <div className="relative">
+              <Image
+                src="/homeTiles/tanker.jpg"
+                alt="Tanker"
+                width={400}
+                height={400}
+                priority={false}
+                placeholder="empty"
+                className="rounded-xl bg-gray-900/5 object-cover shadow-lg"
+              />
+            </div>
+            <div className="relative">
+              <Image
+                src="/homeTiles/factory.jpg"
+                alt="Factory"
+                width={400}
+                height={400}
+                priority={false}
+                placeholder="empty"
+                className="rounded-xl bg-gray-900/5 object-cover shadow-lg"
+              />
+            </div>
+          <div className="lg:w-44 w-28 flex-none space-y-8"></div>
         </div>
       </div>
     </div>
