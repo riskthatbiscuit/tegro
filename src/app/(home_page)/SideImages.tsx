362f029530794ba5ca688e86c96af67f8b18a242
Casey Mcgrath <caseymcgrath1000@gmail.com>
2025-02-06 21:32:37 -0500

    Adding analytics

--- src/app/(home_page)/SideImages.tsx
+++ src/app/(home_page)/SideImages.tsx
@@ -1,67 +1,55 @@
+import Image from 'next/image';
+
 export default function SideImages() {
   return (
     <div className="hidden w-full lg:pt-24 md:block">
       <div className="flex items-center justify-start gap-4">
         <div className="lg:w-44 w-28 flex-none space-y-8 pt-44">
           <div className="relative flex justify-center">
-            <img
-              src="./homeTiles/Batteries.jpg"
-              alt=""
-              className="aspect-[3/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+            <Image
+              src="/homeTiles/Batteries.jpg"
+              alt="Batteries"
+              width={400}
+              height={400}
+              priority={false}
+              placeholder="empty"
+              className="rounded-xl bg-gray-900/5 object-cover shadow-lg"
             />
-            <div className="pointer-events-none items-center justify-center rounded-xl" />
           </div>
           <div className="relative">
-            <img
-              src="./homeTiles/corn-field.jpg"
-              alt=""
-              className="aspect-[3/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+            <Image
+              src="/homeTiles/corn-field.jpg"
+              alt="Corn Field"
+              width={400}
+              height={400}
+              priority={false}
+              placeholder="empty"
+              className="rounded-xl bg-gray-900/5 object-cover shadow-lg"
             />
-            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
           </div>
         </div>
         <div className="lg:w-44 w-28 flex-none space-y-8">
           <div className="relative">
-            <img
-              src="./homeTiles/airplane.jpg"
-              alt=""
-              className="aspect-[3/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+            <Image
+              src="/homeTiles/airplane.jpg"
+              alt="Airplane"
+              width={400}
+              height={400}
+              priority={false}
+              placeholder="empty"
+              className="rounded-xl bg-gray-900/5 object-cover shadow-lg"
             />
-            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
           </div>
           <div className="relative">
-            <img
-              src="./homeTiles/solar.jpg"
-              alt=""
-              className="aspect-[3/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+            <Image
+              src="/homeTiles/solar.jpg"
+              alt="Solar Panels"
+              width={400}
+              height={400}
+              priority={false}
+              placeholder="empty"
+              className="rounded-xl bg-gray-900/5 object-cover shadow-lg"
             />
-            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-          </div>
-        </div>
-        <div className="lg:w-44 w-28 flex-none space-y-8 ">
-          <div className="relative">
-            <img
-              src="./homeTiles/manufacturing.webp"
-              alt=""
-              className="aspect-[3/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-            />
-            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-          </div>
-          <div className="relative">
-            <img
-              src="./homeTiles/tanker.jpg"
-              alt=""
-              className="aspect-[3/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-            />
-            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-          </div>
-          <div className="relative">
-            <img
-              src="./homeTiles/factory.jpg"
-              alt=""
-              className="aspect-[3/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-            />
-            <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
           </div>
         </div>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { ModalProvider } from './components/modals/ModalContext';
 import GridBackground from './components/backgrounds/grid3';
+import { Analytics } from '@vercel/analytics/react';
 
 export default async function RootLayout({
   children,
